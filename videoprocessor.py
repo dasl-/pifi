bfeaffe08fcae3fe5fec193176522cc8a86f358a
LesserChance <lesserchance@gmail.com>
2019-05-28 02:13:08 +0100

    refactor -- add FPS to filename because np.save on the shared mem frames array doesnt support adding the FPS to the saved array

--- videoprocessor.py
+++ videoprocessor.py
@@ -7,39 +7,70 @@ import os
 import time
 import sys
 import urllib
+import re
 
 class VideoProcessor:
 
+    # Settings
     __video_settings = None
+
+    # pafy video stream
     __video_stream = None
-    __frames_save_path = None
 
     __thumbnail_url = None
     __thumbnail = None
 
-    __video_fps = None
-    __video_frames = []
-
     __DATA_DIRECTORY = 'lightness_data'
-    __DATA_FILE_FORMAT = '%title@%resolution__%color.%extension.npy'
-    __VIDEO_FILE_FORMAT = '%title@%resolution.%extension'
+    __FRAMES_FILE_FORMAT = '%s@%s__%s.%s.npy'
+    __VIDEO_FILE_FORMAT = '%s@%s.%s'
 
     def __init__(self, url, video_settings):
         self.__video_settings = video_settings
         self.__video_stream = self.__get_video_stream(url)
-        self.__frames_save_path = self.__get_frames_save_path()
-
-        print("frames path: " + self.__frames_save_path)
 
     def preprocess_and_play(self, video_player):
         self.__get_and_display_thumbnail(video_player)
 
-        if os.path.exists(self.__frames_save_path):
-            self.__video_fps, self.__video_frames = np.load(self.__frames_save_path)
+        existing_frames_file_name, video_fps = self.__get_existing_frames_file_name()
+        if existing_frames_file_name:
+            existing_frames_path = self.__get_data_directory() + "/" + existing_frames_file_name
+            print("Found existing frames file: " + existing_frames_path)
+            video_frames = np.load(self.__get_data_directory() + "/" + existing_frames_file_name)
         else:
-            self.__process_video()
+            print("Unable to find existing frames file. Processing video...")
+            video_frames, video_fps = self.__process_video()
 
-        video_player.playVideo(self.__video_frames, self.__video_fps)
+        video_player.playVideo(video_frames, video_fps)
+
+    # Regex match from frames save pattern to see if a file matches that with an unknown FPS.
+    def __get_existing_frames_file_name(self):
+        regex = self.__insert_fps_into_frames_file_name('([0-9.]+)', True)
+        regex = "^" + regex + "$"
+        regex = re.compile(regex)
+
+        for file in os.scandir(self.__get_data_directory()):
+            if not file.is_file():
+                continue
+            match = regex.match(file.name)
+            if match:
+                fps = match.group(1)
+                return [file.name, float(fps)]
+
+        return None, None
+
+    # we insert the video fps into the saved frames file name
+    def __insert_fps_into_frames_file_name(self, fps, should_regex_escape):
+        frames_save_pattern = self.__get_frames_save_pattern()
+        dot_char = '.'
+        if should_regex_escape:
+            frames_save_pattern = re.escape(frames_save_pattern)
+            dot_char = '\\.'
+
+        index_of_last_dot = frames_save_pattern.rfind(dot_char)
+        chopped = frames_save_pattern[:index_of_last_dot]
+        index_of_penultimate_dot = chopped.rfind(dot_char)
+        frames_file_name = frames_save_pattern[:index_of_penultimate_dot] + "__" + fps + "fps" + frames_save_pattern[index_of_penultimate_dot:]
+        return frames_file_name
 
     def __get_and_display_thumbnail(self, video_player):
         try:
@@ -65,8 +96,12 @@ class VideoProcessor:
 
         print("Options:")
         for stream in p.videostreams:
+            filesize = '??'
+            if (stream._info['filesize']):
+                filesize = str(round(stream._info['filesize']/1024/1024, 2))
+
             print("    " + stream.extension + "@" + stream.resolution + " " +
-                str(stream._info['fps']) + "fps " + str(round(stream._info['filesize']/1024/1024, 2)) + "MB")
+                str(stream._info['fps']) + "fps " + filesize + "MB")
             if (not best_stream or
                 (
                     stream.dimensions[0] <= lowest_x_dimension and
@@ -82,21 +117,22 @@ class VideoProcessor:
 
         return best_stream
 
-    def __save_frames(self):
+    def __save_frames(self, video_frames, video_fps):
         np.save(
-            self.__frames_save_path,
-            [self.__video_fps, self.__video_frames]
+            self.__get_data_directory() + "/" + self.__insert_fps_into_frames_file_name(str(video_fps), False),
+            video_frames
         )
 
-    def __get_frames_save_path(self):
-        filename = self.__DATA_FILE_FORMAT \
-            .replace('%title', self.__video_stream.title) \
-            .replace('%resolution', self.__video_stream.resolution) \
-            .replace('%color', "color" if self.__video_settings.is_color else "bw") \
-            .replace('%extension', self.__video_stream.extension)
+    # get the frames save file name without the FPS inserted into it
+    def __get_frames_save_pattern(self):
+        filename = self.__FRAMES_FILE_FORMAT % (
+            self.__video_stream.title,
+            self.__video_stream.resolution,
+            "color" if self.__video_settings.is_color else "bw",
+            self.__video_stream.extension
+        )
 
-        save_path = (self.__get_data_directory() + "/" + filename)
-        return save_path
+        return filename
 
     # download video rather than streaming to avoid errors like:
     # [tls @ 0x1388940] Error in the pull function.
@@ -104,17 +140,22 @@ class VideoProcessor:
     # [tls @ 0x1388940] The specified session has been invalidated for some reason.
     # [tls @ 0x1388940] The specified session has been invalidated for some reason.
     def __download_video(self):
-        filename = self.__VIDEO_FILE_FORMAT \
-            .replace('%title', self.__video_stream.title) \
-            .replace('%resolution', self.__video_stream.resolution) \
-            .replace('%extension', self.__video_stream.extension)
+        filename = self.__VIDEO_FILE_FORMAT % (
+            self.__video_stream.title,
+            self.__video_stream.resolution,
+            self.__video_stream.extension
+        )
 
         save_path = (self.__get_data_directory() + "/" + filename)
 
-        print("video path: " + save_path)
-        if not os.path.exists(save_path):
+        if os.path.exists(save_path):
+            print("Found existing video file: " + save_path)
+            return save_path
+        else:
+            print("Unable to find existing video file. Downloading video...")
             self.__video_stream.download(save_path)
-        return save_path
+            return save_path
+
 
     def __get_data_directory(self):
         save_dir = sys.path[0] + "/" + self.__DATA_DIRECTORY
@@ -133,13 +174,13 @@ class VideoProcessor:
             return False, None
         return success, frame
 
-    def __process_video(self, stream=False, video_player=None):
+    def __process_video(self):
         video_path = self.__download_video()
 
         # start the video
         vid_cap = cv2.VideoCapture(video_path)
 
-        fps = vid_cap.get(cv2.CAP_PROP_FPS)
+        video_fps = vid_cap.get(cv2.CAP_PROP_FPS)
         frame_width = vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH)
         frame_height = vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
         slice_width = (frame_width / self.__video_settings.display_width)
@@ -147,9 +188,9 @@ class VideoProcessor:
 
         video_frames = []
 
-        start = time.time()
         i = 0
-        log_freq = 1/200
+        log_freq = 1 / 200
+        start = time.time()
         while True:
             success, frame = self.__get_next_frame(vid_cap)
             if not success:
@@ -158,26 +199,25 @@ class VideoProcessor:
             if not self.__video_settings.is_color:
                 frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
-            if i % int(round(log_freq ** -1)) == 0 and i != 0:
-                print("Processing frame at " + str(vid_cap.get(cv2.CAP_PROP_POS_MSEC)) + " ms.")
-                print ("Processing at " + str(i / (time.time() - start)) + " frames per second.")
+            if i != 0 and i % int(round(log_freq ** -1)) == 0:
+                processing_frame_sec = str(round(vid_cap.get(cv2.CAP_PROP_POS_MSEC) / 1000, 2))
+                processing_fps = str(round(i / (time.time() - start), 2))
+                print("Processing frame " + processing_frame_sec + " s at " + processing_fps + " frames per second.")
                 # print(frame.shape)
 
             avg_color_frame = self.__get_avg_color_frame(slice_width, slice_height, frame)
             video_frames.append(avg_color_frame)
 
-            if stream:
-                video_player.playFrame(avg_color_frame)
             i += 1
 
         end = time.time()
-        print("processing video took: " + str(end - start) + " seconds")
+        print("processing video took: " + str(round(end - start, 2)) + " seconds")
 
         vid_cap.release()
         cv2.destroyAllWindows()
 
-        if not os.path.exists(self.__frames_save_path):
-            self.__save_frames()
+        self.__save_frames(video_frames, video_fps)
+        return video_frames, video_fps
 
     def __get_avg_color_frame(self, slice_width, slice_height, frame):
         if self.__video_settings.is_color:
