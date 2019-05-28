0bee73b5d62994d3fe5bab05607924e6155a9c52
LesserChance <lesserchance@gmail.com>
2019-05-28 04:40:33 +0100

    parallel video processing

--- videoprocessor.py
+++ videoprocessor.py
@@ -8,6 +8,8 @@ import time
 import sys
 import urllib
 import re
+import multiprocessing
+import sharedmem
 
 class VideoProcessor:
 
@@ -38,7 +40,7 @@ class VideoProcessor:
             video_frames = np.load(self.__get_data_directory() + "/" + existing_frames_file_name)
         else:
             print("Unable to find existing frames file. Processing video...")
-            video_frames, video_fps = self.__process_video()
+            video_frames, video_fps = self.__process_video(video_player)
 
         video_player.playVideo(video_frames, video_fps)
 
@@ -162,10 +164,10 @@ class VideoProcessor:
         os.makedirs(save_dir, exist_ok=True)
         return save_dir
 
-    def __get_next_frame(self, vid_cap):
+    def __get_next_frame(self, vid_cap, num_skip_frames):
         success = True
         # add one because we need to call .grab() once even if we're skipping no frames.
-        for x in range(1):
+        for x in range(num_skip_frames + 1):
             success = vid_cap.grab()
             if not success:
                 return False, None
@@ -174,57 +176,96 @@ class VideoProcessor:
             return False, None
         return success, frame
 
-    def __process_video(self):
-        video_path = self.__download_video()
-
-        # start the video
+    def __process_video_subprocess(self, avg_color_frames, process_num, num_processes, video_path):
+        print("[SUBPROCESS " + str(process_num) + "] Starting...")
+        start_frame = process_num
         vid_cap = cv2.VideoCapture(video_path)
+        success, frame = self.__get_next_frame(vid_cap, start_frame)
+        if not success:
+            return
 
-        video_fps = vid_cap.get(cv2.CAP_PROP_FPS)
-        frame_width = vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-        frame_height = vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        slice_width = (frame_width / self.__video_settings.display_width)
-        slice_height = (frame_height / self.__video_settings.display_height)
+        if (self.__video_settings.is_color):
+            num_frames, display_width, display_height, ignore = avg_color_frames.shape
+        else:
+            num_frames, display_width, display_height = avg_color_frames.shape
 
-        video_frames = []
-
-        i = 0
+        slice_width = vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH) / display_width
+        slice_height = vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT) / display_height
+        current_frame = start_frame
+        loop_counter = 0
         log_freq = 1 / 200
         start = time.time()
-        while True:
-            success, frame = self.__get_next_frame(vid_cap)
-            if not success:
-                break
-
+        while (True):
             if not self.__video_settings.is_color:
                 frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
-            if i != 0 and i % int(round(log_freq ** -1)) == 0:
+            if loop_counter != 0 and loop_counter % int(round(log_freq ** -1)) == 0:
                 processing_frame_sec = str(round(vid_cap.get(cv2.CAP_PROP_POS_MSEC) / 1000, 2))
-                processing_fps = str(round(i / (time.time() - start), 2))
-                print("Processing frame " + processing_frame_sec + " s at " + processing_fps + " frames per second.")
-                # print(frame.shape)
+                processing_fps = str(round(loop_counter / (time.time() - start), 2))
+                print("[SUBPROCESS " + str(process_num) + "] Progress: " + str(round(current_frame / num_frames * 100, 2)) + "%. " +
+                    "Processing frame from " + processing_frame_sec + "s at " + processing_fps + " frames per second.")
 
             avg_color_frame = self.__get_avg_color_frame(slice_width, slice_height, frame)
-            video_frames.append(avg_color_frame)
+            avg_color_frames[current_frame] = avg_color_frame
+            current_frame += num_processes
+            if current_frame < num_frames:
+                # some videos report an incorrect frame count (less frames than it actually is).
+                # Make sure we don't go over what we thought it was...
+                success, frame = self.__get_next_frame(vid_cap, num_processes - 1)
+                if not success:
+                    print("Unable to __get_next_frame")
+                    break
+            else:
+                break
 
-            i += 1
-
-        end = time.time()
-        print("processing video took: " + str(round(end - start, 2)) + " seconds")
+            loop_counter += 1
 
         vid_cap.release()
-        cv2.destroyAllWindows()
+        end = time.time()
+        processing_fps = str(round(loop_counter / (end - start), 2))
+        print("[SUBPROCESS " + str(process_num) + "] Processing video subprocess completed in " +
+            str(round(end - start, 2)) + " seconds at " + processing_fps + " frames per second.")
 
-        self.__save_frames(video_frames, video_fps)
-        return video_frames, video_fps
+    def __process_video(self, video_player):
+        video_path = self.__download_video()
+        vid_cap = cv2.VideoCapture(video_path)
+        video_fps = vid_cap.get(cv2.CAP_PROP_FPS)
+        num_frames = int(vid_cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        vid_cap.release()
+
+        avg_color_frames_shape = self.__get_avg_color_frame_shape()
+        avg_color_frames_shape.insert(0, num_frames)
+        avg_color_frames = sharedmem.empty(avg_color_frames_shape, dtype=np.uint8)
+        num_processes = multiprocessing.cpu_count()
+        processes = []
+        start = time.time()
+        for process_num in range(num_processes):
+            processes.append(sharedmem.background(
+                function = self.__process_video_subprocess,
+                avg_color_frames = avg_color_frames,
+                process_num = process_num,
+                num_processes = num_processes,
+                video_path = video_path
+            ))
+
+        # time.sleep(7)
+        # video_player.playVideo(avg_color_frames, video_fps)
+        for process in processes:
+            process.wait()
+
+        print("Processing whole video completed in " + str(round(time.time() - start, 2)) + " seconds.")
+
+        self.__save_frames(avg_color_frames, video_fps)
+        return avg_color_frames, video_fps
+
+    def __get_avg_color_frame_shape(self):
+        if self.__video_settings.is_color:
+            return [self.__video_settings.display_width, self.__video_settings.display_height, 3]
+        else:
+            return [self.__video_settings.display_width, self.__video_settings.display_height]
 
     def __get_avg_color_frame(self, slice_width, slice_height, frame):
-        if self.__video_settings.is_color:
-            avg_color_frame = np.empty((self.__video_settings.display_width, self.__video_settings.display_height, 3), np.uint8)
-        else:
-            avg_color_frame = np.empty((self.__video_settings.display_width, self.__video_settings.display_height), np.uint8)
-
+        avg_color_frame = np.empty(self.__get_avg_color_frame_shape(), np.uint8)
         for x in range(self.__video_settings.display_width):
             for y in range(self.__video_settings.display_height):
                 mask = np.zeros(frame.shape[:2], np.uint8)
