a37eede809f3a29471b36ea8d10a3f8a36f872f0
Ryan Bateman <lesserchance@gmail.com>
2019-06-18 21:13:37 -0400

    nope, that was broken

--- dependencies.sh
+++ dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo apt-get update
 sudo apt-get -y install git vim python3-pip libilmbase-dev libopenexr-dev libgstreamer1.0-dev libtiff5-dev libjasper-dev libpng-dev libjpeg-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libgtk2.0-dev libatlas-base-dev gfortran libgdk-pixbuf2.0-dev libpango1.0-dev libcairo2-dev libqtgui4 libqt4-test
-sudo pip3 install --upgrade youtube_dl numpy opencv-python sharedmem HTTPServer Adafruit_GPIO
+sudo pip3 install --upgrade youtube_dl numpy opencv-python sharedmem HTTPServer
