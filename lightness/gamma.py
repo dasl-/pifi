aadf6ecdc3c861d70bf822d10e3933c9b47e8b12
dasl <david.leibovic@gmail.com>
2019-07-06 19:47:31 +0100

    make things private

--- lightness/gamma.py
+++ lightness/gamma.py
@@ -2,26 +2,26 @@ import numpy as np
 
 class Gamma:
     # possible gamma curve range, step by .1
-    MIN_GAMMA_CURVE = 2
-    MAX_GAMMA_CURVE = 6
+    __MIN_GAMMA_CURVE = 2
+    __MAX_GAMMA_CURVE = 6
 
     # % to scale down the max brightness of each individual led
-    RED_MAX_BRIGHTNESS = 1
-    GREEN_MAX_BRIGHTNESS = .45
-    BLUE_MAX_BRIGHTNESS = .375
+    __RED_MAX_BRIGHTNESS = 1
+    __GREEN_MAX_BRIGHTNESS = .45
+    __BLUE_MAX_BRIGHTNESS = .375
 
     # list of gamma curves from min to max
     scale_red = []
     scale_blue = []
     scale_green = []
 
-    video_settings = None
+    __video_settings = None
 
     gamma_index = 18
 
     def __init__(self, video_settings):
-        self.video_settings = video_settings
-        self.generateGammaScales()
+        self.__video_settings = video_settings
+        self.__generateGammaScales()
 
     def getScaledRGBOutputForColorFrame(self, frame, x, y):
         return [
@@ -49,11 +49,6 @@ class Gamma:
 
         return gamma_scale[self.gamma_index][int(brightness)]
 
-    # set a specific index (0=min)
-    def setGammaIndex(self, new_gamma_index):
-        self.gamma_index = new_gamma_index
-        return new_gamma_index
-
     # powers auto gamma curve using the average brightness of the given frame
     def setGammaIndexForFrame(self, frame):
         self.gamma_index = self.getGammaIndexByMagicForFrame(frame)
@@ -69,8 +64,8 @@ class Gamma:
 
         if gamma_index < 0:
             return 0
-        elif gamma_index >= ((self.MAX_GAMMA_CURVE - self.MIN_GAMMA_CURVE) * 10) - 1:
-            return ((self.MAX_GAMMA_CURVE - self.MIN_GAMMA_CURVE) * 10) - 1
+        elif gamma_index >= ((self.__MAX_GAMMA_CURVE - self.__MIN_GAMMA_CURVE) * 10) - 1:
+            return ((self.__MAX_GAMMA_CURVE - self.__MIN_GAMMA_CURVE) * 10) - 1
         else:
             return int(round(gamma_index))
 
@@ -78,7 +73,8 @@ class Gamma:
     # gamma: Correction factor
     # max_in: Top end of INPUT range
     # max_out: Top end of OUTPUT range
-    def getGammaScaleValues(self, gamma, max_in, max_out):
+    # https://learn.adafruit.com/led-tricks-gamma-correction/
+    def __getGammaScaleValues(self, gamma, max_in, max_out):
         gamma_list = []
         for i in range(0, max_in+1):
             gamma_list.append(
@@ -91,17 +87,16 @@ class Gamma:
 
         return gamma_list
 
-    # is_color: true for color video
-    def generateGammaScales(self):
-        for i in range(self.MIN_GAMMA_CURVE * 10, self.MAX_GAMMA_CURVE * 10):
-            self.scale_red.append(self.getGammaScaleValues(i/10, 255, int(255 * self.RED_MAX_BRIGHTNESS)))
-            self.scale_blue.append(self.getGammaScaleValues(i/10, 255, int(255 * self.BLUE_MAX_BRIGHTNESS)))
-            self.scale_green.append(self.getGammaScaleValues(i/10, 255, int(255 * self.GREEN_MAX_BRIGHTNESS)))
+    def __generateGammaScales(self):
+        for i in range(self.__MIN_GAMMA_CURVE * 10, self.__MAX_GAMMA_CURVE * 10):
+            self.scale_red.append(self.__getGammaScaleValues(i/10, 255, int(255 * self.__RED_MAX_BRIGHTNESS)))
+            self.scale_blue.append(self.__getGammaScaleValues(i/10, 255, int(255 * self.__BLUE_MAX_BRIGHTNESS)))
+            self.scale_green.append(self.__getGammaScaleValues(i/10, 255, int(255 * self.__GREEN_MAX_BRIGHTNESS)))
 
         # for black and white, if r, g, or b has a zero in the scale they all should be 0
         # otherwise dim pixels will be just that color
-        if not self.video_settings.is_color:
-            for g in range(0, ((self.MAX_GAMMA_CURVE - self.MIN_GAMMA_CURVE) * 10)):
+        if not self.__video_settings.is_color:
+            for g in range(0, ((self.__MAX_GAMMA_CURVE - self.__MIN_GAMMA_CURVE) * 10)):
                 for i in range(0, 256):
                     if min(self.scale_red[g][i], self.scale_green[g][i], self.scale_blue[g][i]) == 0:
                         self.scale_red[g][i] = 0
