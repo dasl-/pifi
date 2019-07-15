7c55c131ef9d1e20e633d41d6aca137693a057f6
lesserchance <lesserchance@gmail.com>
2019-07-15 05:30:51 +0100

    Move global config to central location

---
+++ lightness/config.py
@@ -0,0 +1,42 @@
+import os
+import json
+from lightness.logger import Logger
+
+class Config:
+
+    __custom_server_args = None
+    __custom_video_args = None
+
+    __CONFIG_FILENAME = 'config.json'
+    __logger = None
+
+    def __init__(self):
+        self.__logger = Logger().set_namespace(self.__class__.__name__)
+        self.__maybe_load_custom_config()
+
+    def getServerConfig(self, key, default=None):
+        if self.__custom_server_args:
+            if key in self.__custom_server_args:
+                return self.__custom_server_args[key]
+
+        return default
+
+    def getCustomVideoArgs(self):
+        if self.__custom_video_args:
+            return self.__custom_video_args
+
+        return []
+
+    def __maybe_load_custom_config(self):
+        config_path = '/home/pi/lightness/' + self.__CONFIG_FILENAME
+        if not os.path.exists(config_path):
+            self.__logger.info("No custom config found at: {}".format(config_path))
+            return
+
+        self.__logger.info("Found custom config found at: {}".format(config_path))
+        with open(config_path) as config_json:
+            data = json.load(config_json)
+            if 'custom_server_args' in data:
+                self.__custom_server_args = data['custom_server_args']
+            if 'custom_video_args' in data:
+                self.__custom_video_args = data['custom_video_args']
