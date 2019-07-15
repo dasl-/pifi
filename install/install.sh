c8e3817fd224037ed18d786fb6d91e353d515219
dasl <david.leibovic@gmail.com>
2019-07-15 08:34:36 +0100

    update install script to generate loading screens if they dont already exist

--- install/install.sh
+++ install/install.sh
@@ -1,5 +1,42 @@
 #!/bin/bash
+
+usage() {
+  local exit_code=$1
+  echo "usage: $0 -w <display width> -l <display height>"
+  echo "    -h                   display this help message"
+  echo "    -w <display width>   Num of LEDs in the array horizontally. Defaults to 28."
+  echo "    -l <display height>  Num of LEDs in the array vertically. Defaults to 18."
+  exit "$exit_code"
+}
+
+# get opts
+display_width=28
+display_height=18
+while getopts ":hw:l:" opt; do
+  case $opt in
+    h) usage 0 ;;
+    w) display_width=$OPTARG ;;
+    l) display_height=$OPTARG ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      usage 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      usage 1
+      ;;
+  esac
+done
+
 set -x
+
+# generate loading screens
+if [ ! -f /home/pi/lightness/loading_screen_monochrome.npy ]; then
+    /home/pi/lightness/utils/img_to_led --image /home/pi/lightness/utils/loading_screen_monochrome.jpg --display-width $display_width --display-height $display_height --output-file /home/pi/lightness/loading_screen --color-mode monochrome
+fi
+if [ ! -f /home/pi/lightness/loading_screen_color.npy ]; then
+    /home/pi/lightness/utils/img_to_led --image /home/pi/lightness/utils/loading_screen_color.jpg --display-width $display_width --display-height $display_height --output-file /home/pi/lightness/loading_screen --color-mode color
+fi
 
 # setup logging: syslog
 sudo mkdir -p /var/log/lightness
