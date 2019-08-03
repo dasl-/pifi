6242fd4c105e3233074cf64f889ff56e521c72ba
Ryan Bateman <lesserchance@gmail.com>
2019-08-03 23:30:30 +0100

    make video duration parser accept videos >= 1 day long

--- app/src/utils.js
+++ app/src/utils.js
@@ -39,15 +39,16 @@ export default {
   },
 
   convertISO8601ToSeconds(input) {
-    var reptms = /^PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?$/;
-    var hours = 0, minutes = 0, seconds = 0, totalseconds;
+    var reptms = /^P(?:(\d+)D)?T(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?$/;
+    var days = 0, hours = 0, minutes = 0, seconds = 0, totalseconds;
 
     if (reptms.test(input)) {
         var matches = reptms.exec(input);
-        if (matches[1]) hours = Number(matches[1]);
-        if (matches[2]) minutes = Number(matches[2]);
-        if (matches[3]) seconds = Number(matches[3]);
-        totalseconds = (hours * 3600)  + (minutes * 60) + seconds;
+        if (matches[1]) days = Number(matches[1]);
+        if (matches[2]) hours = Number(matches[2]);
+        if (matches[3]) minutes = Number(matches[3]);
+        if (matches[4]) seconds = Number(matches[4]);
+        totalseconds = (days * 24 * 3600) + (hours * 3600)  + (minutes * 60) + seconds;
     }
 
     var show_minutes, show_seconds;
