b001d1ca7e3d6420421f14b50f201b5fb48ecbfb
dasl <david.leibovic@gmail.com>
2020-07-19 22:32:47 +0100

    get rid of firefox samesite cookie warnings

--- app/src/utils.js
+++ app/src/utils.js
@@ -2,7 +2,7 @@ function setCookie(cname, cvalue, exhours) {
   var d = new Date();
   d.setTime(d.getTime() + (exhours*60*60*1000));
   var expires = "expires="+ d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;samesite=strict";
 }
 
 function getCookie(cname) {
