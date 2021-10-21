f9cb1fbe744b070b333c267e34fc451f3fd38ea0
dasl <david.leibovic@gmail.com>
2021-10-21 03:41:49 +0000

    remove usage of yt-dlp extractor-args to explicitly send android client user agent as per https://github.com/ytdl-org/youtube-dl/issues/29326#issuecomment-942652530

--- pifi/videoprocessor.py
+++ pifi/videoprocessor.py
@@ -1,10 +1,7 @@
 import numpy as np
-import pprint
 import time
 import os
-import time
 import sys
-import re
 from pifi.logger import Logger
 from pifi.datastructure.readoncecircularbuffer import ReadOnceCircularBuffer
 from pifi.settings.videosettings import VideoSettings
@@ -237,7 +234,6 @@ class VideoProcessor:
 
         return (
             'yt-dlp ' +
-            '--extractor-args youtube:player_client=android ' + # workaround for download speed throttling problem: https://github.com/ytdl-org/youtube-dl/issues/29326#issuecomment-879256177
             '--output \'' + shlex.quote(self.__get_video_save_path()) + "' "
             '--restrict-filenames ' + # get rid of a warning ytdl gives about special chars in file names
             '--format ' + shlex.quote(video_info['format_id']) + " " + # download the specified video quality / encoding
@@ -359,7 +355,6 @@ class VideoProcessor:
             log_opts = '--newline '
         return (
             'yt-dlp ' +
-            '--extractor-args youtube:player_client=android ' + # workaround for download speed throttling problem: https://github.com/ytdl-org/youtube-dl/issues/29326#issuecomment-879256177
             '--output - ' + # output to stdout
             '--restrict-filenames ' + # get rid of a warning ytdl gives about special chars in file names
             '--format ' + shlex.quote(video_info['format_id']) + " " + # download the specified video quality / encoding
