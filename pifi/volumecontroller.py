e9825b138873f9a6573b81aac62c73c534571350
dasl <david.leibovic@gmail.com>
2022-04-29 03:56:19 +0000

    Incorporate VolumeController improvements from piwall, and fix for amixer no longer properly handling fractional percentages for volume setting
    On older kernel, fractional percents are handled fine:
    
    [piwall] ~ % uname -a
    Linux piwall 5.10.63-v7l+ #1496 SMP Wed Dec 1 15:58:56 GMT 2021 armv7l GNU/Linux
    [piwall] ~ % amixer cset numid=1 96%
    numid=1,iface=MIXER,name='Headphone Playback Volume'
      ; type=INTEGER,access=rw---R--,values=1,min=-10239,max=400,step=0
      : values=-25
      | dBscale-min=-102.39dB,step=0.01dB,mute=1
    [piwall] ~ % amixer cget numid=1
    numid=1,iface=MIXER,name='Headphone Playback Volume'
      ; type=INTEGER,access=rw---R--,values=1,min=-10239,max=400,step=0
      : values=-25
      | dBscale-min=-102.39dB,step=0.01dB,mute=1
    [piwall] ~ % amixer cset numid=1 96.9%
    numid=1,iface=MIXER,name='Headphone Playback Volume'
      ; type=INTEGER,access=rw---R--,values=1,min=-10239,max=400,step=0
      : values=71
      | dBscale-min=-102.39dB,step=0.01dB,mute=1
    [piwall] ~ % amixer cget numid=1
    numid=1,iface=MIXER,name='Headphone Playback Volume'
      ; type=INTEGER,access=rw---R--,values=1,min=-10239,max=400,step=0
      : values=71
      | dBscale-min=-102.39dB,step=0.01dB,mute=1
    
    But on a newer kernel, fractional percents are not respected -- it seems to floor the value to an integer:
    
    [pifi] ~/development/pifi % uname -a
    Linux pifi 5.15.32-v7+ #1538 SMP Thu Mar 31 19:38:48 BST 2022 armv7l GNU/Linux
    [pifi] ~/development/pifi % amixer cset numid=1 96%
    numid=1,iface=MIXER,name='Headphone Playback Volume'
      ; type=INTEGER,access=rw---R--,values=1,min=-10239,max=400,step=0
      : values=-26
      | dBscale-min=-102.39dB,step=0.01dB,mute=1
    [pifi] ~/development/pifi % amixer cget numid=1
    numid=1,iface=MIXER,name='Headphone Playback Volume'
      ; type=INTEGER,access=rw---R--,values=1,min=-10239,max=400,step=0
      : values=-26
      | dBscale-min=-102.39dB,step=0.01dB,mute=1
    [pifi] ~/development/pifi % amixer cset numid=1 96.9%
    numid=1,iface=MIXER,name='Headphone Playback Volume'
      ; type=INTEGER,access=rw---R--,values=1,min=-10239,max=400,step=0
      : values=-26
      | dBscale-min=-102.39dB,step=0.01dB,mute=1
    [pifi] ~/development/pifi % amixer cget numid=1
    numid=1,iface=MIXER,name='Headphone Playback Volume'
      ; type=INTEGER,access=rw---R--,values=1,min=-10239,max=400,step=0
      : values=-26
      | dBscale-min=-102.39dB,step=0.01dB,mute=1
    
    Because of these changes, let's set the volume using decibel levels rather than percents.

--- pifi/volumecontroller.py
+++ pifi/volumecontroller.py
@@ -2,55 +2,79 @@ import subprocess
 import re
 import math
 
+# Gets and sets alsa volume
 class VolumeController:
 
-    """
-    sudo amixer cset numid=1 96.24%
-    numid=1,iface=MIXER,name='Headphone Playback Volume'
-      ; type=INTEGER,access=rw---R--,values=1,min=-10239,max=400,step=0
-      : values=0
-      | dBscale-min=-102.39dB,step=0.01dB,mute=1
-
-    Setting 96.24% is equivalent to 0dB. Anything higher may result in clipping.
-    """
+    # Anything higher than 0 dB may result in clipping.
     __LIMITED_MAX_VOL_VAL = 0
 
     # amixer output: ; type=INTEGER,access=rw---R--,values=1,min=-10239,max=400,step=0
-    __GLOBAL_MIN_VOL_VAL = -10239
-    __GLOBAL_MAX_VOL_VAL = 400
+    # These values are in millibels.
+    GLOBAL_MIN_VOL_VAL = -10239
+    GLOBAL_MAX_VOL_VAL = 400
 
     # gets a perceptual loudness %
     # returns a float in the range [0, 100]
     def get_vol_pct(self):
-        res = subprocess.check_output(('amixer', 'cget', 'numid=1')).decode("utf-8")
-        m = re.search(r" values=(-?\d+)", res, re.MULTILINE)
-        if m is None:
+        mb_level = self.get_vol_millibels()
+        if mb_level <= self.GLOBAL_MIN_VOL_VAL:
             return 0
 
-        vol_val = int(m.group(1))
-        if vol_val <= self.__GLOBAL_MIN_VOL_VAL:
-            vol_pct = 0
-        else:
-            # convert from decibel attenuation amount to perceptual loudness %
-            # see: http://www.sengpielaudio.com/calculator-levelchange.htm
-            db_level = vol_val / 100
-            vol_pct = math.pow(2, (db_level / 10)) * 100
-            vol_pct = max(0, vol_pct)
-            vol_pct = min(100, vol_pct)
+        # convert from decibel attenuation amount to perceptual loudness %
+        # see: http://www.sengpielaudio.com/calculator-levelchange.htm
+        db_level = mb_level / 100
+        vol_pct = math.pow(2, (db_level / 10)) * 100
+        vol_pct = max(0, vol_pct)
+        vol_pct = min(100, vol_pct)
         return vol_pct
 
     # takes a perceptual loudness %.
     # vol_pct should be a float in the range [0, 100]
     def set_vol_pct(self, vol_pct):
+        mb_level = round(VolumeController.pct_to_millibels(vol_pct))
+        subprocess.check_output(('amixer', 'cset', 'numid=1', '--', str(mb_level)))
+
+    # increments volume percentage by the specified increment. The increment should be a float in the range [0, 100]
+    # Returns the new volume percent, which will be a float in the range [0, 100]
+    def increment_vol_pct(self, inc = 1):
+        old_vol_pct = self.get_vol_pct()
+        new_vol_pct = old_vol_pct + inc
+        new_vol_pct = max(0, new_vol_pct)
+        new_vol_pct = min(100, new_vol_pct)
+        self.set_vol_pct(new_vol_pct)
+        return new_vol_pct
+
+    # Return volume in millibels. Returns an integer in the range [self.GLOBAL_MIN_VOL_VAL, 0]
+    def get_vol_millibels(self):
+        res = subprocess.check_output(('amixer', 'cget', 'numid=1')).decode("utf-8")
+        m = re.search(r" values=(-?\d+)", res, re.MULTILINE)
+        if m is None:
+            return self.GLOBAL_MIN_VOL_VAL
+
+        mb_level = int(m.group(1))
+        mb_level = max(self.GLOBAL_MIN_VOL_VAL, mb_level)
+        mb_level = min(0, mb_level)
+        return mb_level
+
+    # Map the volume from [0, 100] to [0, 1]
+    @staticmethod
+    def normalize_vol_pct(vol_pct):
+        vol_pct_normalized = vol_pct / 100
+        vol_pct_normalized = max(0, vol_pct_normalized)
+        vol_pct_normalized = min(1, vol_pct_normalized)
+        return vol_pct_normalized
+
+    # input: [0, 100]
+    # output: [self.GLOBAL_MIN_VOL_VAL, 0]
+    @staticmethod
+    def pct_to_millibels(vol_pct):
         if (vol_pct <= 0):
-            db_level = self.__GLOBAL_MIN_VOL_VAL / 100
+            mb_level = VolumeController.GLOBAL_MIN_VOL_VAL
         else:
             # get the decibel adjustment required for the human perceived loudness %.
             # see: http://www.sengpielaudio.com/calculator-levelchange.htm
-            db_level = 10 * math.log(vol_pct / 100, 2)
+            mb_level = 1000 * math.log(vol_pct / 100, 2)
 
-        db_level = max(self.__GLOBAL_MIN_VOL_VAL / 100, db_level)
-        db_level = min(self.__LIMITED_MAX_VOL_VAL, db_level)
-
-        pct_to_set = (((db_level * 100) - self.__GLOBAL_MIN_VOL_VAL) / (self.__GLOBAL_MAX_VOL_VAL - self.__GLOBAL_MIN_VOL_VAL)) * 100
-        subprocess.check_output(('amixer', 'cset', 'numid=1', '{}%'.format(pct_to_set)))
+        mb_level = max(VolumeController.GLOBAL_MIN_VOL_VAL, mb_level)
+        mb_level = min(VolumeController.__LIMITED_MAX_VOL_VAL, mb_level)
+        return mb_level
