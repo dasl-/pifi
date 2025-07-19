27806358f55324b3aad7365479694a3b1e59fd81
Reese Gyllenhammer <rgyllenhammer@gmail.com>
2025-07-19 18:49:53 -0400

    Saved Video Screensavers (#43)
    * initial commit for saved video screensavers
    
    * implementation for video screensavers
    
    * cleanup and removing loading screen
    
    * properly switching videos
    
    * removing unnecessary saved_videos entry in screensavers.screensavers
    
    * more cleanup
    
    ---------
    
    Co-authored-by: Reese Gyllenhammer <rgyllenhammer@reesesmacstudio.mynetworksettings.com>

--- pifi/screensavermanager.py
+++ pifi/screensavermanager.py
@@ -3,6 +3,7 @@ import random
 from pifi.config import Config
 from pifi.games.cellularautomata.cyclicautomaton import CyclicAutomaton
 from pifi.games.cellularautomata.gameoflife import GameOfLife
+from pifi.video.videoscreensaver import VideoScreensaver
 from pifi.led.ledframeplayer import LedFramePlayer
 from pifi.logger import Logger
 
@@ -16,11 +17,14 @@ class ScreensaverManager:
         self.__led_frame_player = LedFramePlayer()
 
         screensaver_types = Config.get("screensavers.screensavers")
+        saved_videos = Config.get("screensavers.saved_videos", [])
         self.__screensavers = []
         if "game_of_life" in screensaver_types:
             self.__screensavers.append(GameOfLife(led_frame_player = self.__led_frame_player))
         if "cyclic_automaton" in screensaver_types:
             self.__screensavers.append(CyclicAutomaton(led_frame_player = self.__led_frame_player))
+        if saved_videos != []:
+            self.__screensavers.append(VideoScreensaver(video_list = saved_videos))
 
     def run(self):
         while True:
---
+++ pifi/video/videoscreensaver.py
@@ -0,0 +1,26 @@
+import os
+import random
+from pifi.config import Config
+from pifi.directoryutils import DirectoryUtils
+from pifi.video.videoprocessor import VideoProcessor
+
+class VideoScreensaver:
+
+    __DATA_DIRECTORY = 'data/screensavers'
+
+    def __init__(self, video_list):
+        self.video_list = video_list
+
+    def __getScreensaverPath(self):
+        save_dir = DirectoryUtils().root_dir + '/' + self.__DATA_DIRECTORY
+        os.makedirs(save_dir, exist_ok=True)
+        return save_dir
+
+    def play(self):
+        url = self.__getScreensaverPath() + '/' + random.choice(self.video_list)
+        VideoProcessor(
+            url = url,
+            clear_screen = True,
+            yt_dlp_extractors = "",
+            show_loading_screen = False
+        ).process_and_play()
