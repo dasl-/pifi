c35010f80b87581f8c260ccb18946ed82012b69d
dasl- <david.leibovic@gmail.com>
2025-10-29 23:49:12 -0400

    cron logging to jorunalctl

--- install/pifi_cron.sh
+++ install/pifi_cron.sh
@@ -2,5 +2,6 @@
 # creates the pifi cron file
 BASE_DIR="$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )")"
 cat <<-EOF | sudo tee /etc/cron.d/pifi >/dev/null
-31 09 * * * root $BASE_DIR/utils/update_yt-dlp.sh
+# View logs via: sudo journalctl -t update_yt_dlp
+31 09 * * * root $BASE_DIR/utils/update_yt-dlp.sh 2>&1 | /usr/bin/logger -t update_yt_dlp
 EOF
